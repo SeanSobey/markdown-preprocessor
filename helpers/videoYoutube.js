e4bc827ff8928c3a5baf5d8cb3040c93c566e015
Sean Sobey SAHL <seans@sahomeloans.com>
2019-03-14 16:46:00 +0200

    Fixed video formatting

--- helpers/videoYoutube.js
+++ helpers/videoYoutube.js
@@ -21,9 +21,9 @@ module.exports = () => {
 			: config.url);
 		const key = url.searchParams.get('v');
 		const markdown = [
-`    <div align="center">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>`
+`<div align="center">
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>`
 		];
 		if (config.timestamps) {
 			const tableHeader = [ [`Time`, `Note`] ];
