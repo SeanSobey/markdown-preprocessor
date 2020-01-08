52ac7812849092657eea82f4ab8f677eb5823f75
Sean Sobey SAHL <seans@sahomeloans.com>
2020-01-08 15:32:58 +0200

    Updated Helper interface

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -1,1 +1,8 @@
-export type Helper = (config: { readonly [key: string]: any }) => string|Promise<string>;
+export type Helper = (config: { readonly [key: string]: any }, context: HelperContext) => string|Promise<string>;
+
+export interface HelperContext {
+	readonly gitdown: any;
+	readonly locator: any;
+	readonly markdown: string;
+	readonly parser: any;
+}
