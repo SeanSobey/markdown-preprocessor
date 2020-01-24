261c96345139325429e4391159bcb55723b83edb
Sean Sobey SAHL <seans@sahomeloans.com>
2020-01-24 09:55:41 +0200

    css for details

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -18,11 +18,11 @@ function createScripts(): ReadonlyArray<string> {
 	return [
 		'<script src="https://www.youtube.com/iframe_api"></script>',
 `<script type="text/javascript">
-	window.YouTubeIframeAPIReadyCallbacks = [];
-	window.YouTubePlayers = {};
-	function onYouTubeIframeAPIReady() {
-		window.YouTubeIframeAPIReadyCallbacks.forEach((fn) => fn());
-	}
+    window.YouTubeIframeAPIReadyCallbacks = [];
+    window.YouTubePlayers = {};
+    function onYouTubeIframeAPIReady() {
+        window.YouTubeIframeAPIReadyCallbacks.forEach((fn) => fn());
+    }
 </script>`
 	];
 }
@@ -30,6 +30,11 @@ function createScripts(): ReadonlyArray<string> {
 function createStyles(): ReadonlyArray<string> {
 
 	return [
-		`<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">${lineBreak}`,
+`<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">${lineBreak}
+<style>
+    details {
+        margin-bottom: 20px;
+    }
+</style>`,
 	];
 }
